7d5bb2849d25abff13d8246a060ae3f7ce5d229b
shamimspro <shamimspro@gmail.com>
2022-10-28 19:01:53 +0600

    create file folder structure

---
+++ src/contexts/UserContext.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UserContext = () => {
+    return (
+        <div>
+            <h2>UserContext page</h2>
+        </div>
+    );
+};
+
+export default UserContext;
